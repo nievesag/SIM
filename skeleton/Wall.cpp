33e6a5ea457785fd77699b5418ac1c663ebc4a41
nievesag <nievalon@ucm.es>
2025-11-09 00:33:41 +0100

    huecos

--- skeleton/Wall.cpp
+++ skeleton/Wall.cpp
@@ -5,13 +5,8 @@ Wall::Wall(Scene* scn, int size, Vector3 pos, bool empty)
 {
 	pose = new physx::PxTransform(pos);
 	shape = CreateShape(physx::PxBoxGeometry(size, size, size));
-	if (!isEmpty)
-	{
-		color = { 0.5,0.8,0.03,1 };
-	}
-	else
-	{
-		color = { 1,0,0,0.5};
-	}
+	if (!isEmpty) color = { 0.5,0.8,0.03,1 };
+	else  color = { 1,0,0,0.5};
 	renderItem = new RenderItem(shape, pose, color);
+	if (isEmpty) DeregisterRenderItem(renderItem);
 }
