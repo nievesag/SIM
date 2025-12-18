07c7ada7933c4b48d19195101229cd8df0629601
nievalon <nievalon@ucm.es>
2025-12-19 00:50:05 +0100

    Update Scene.cpp

--- skeleton/Scene.cpp
+++ skeleton/Scene.cpp
@@ -546,7 +546,7 @@ void Scene2::init()
 	addEntity(pipe);
 
 	ParticleGenerator* wGenerator = new MistGenerator(this, "Niebla");
-	static_cast<MistGenerator*>(wGenerator)->setOrigin({ 150, 80, 0 });
+	static_cast<MistGenerator*>(wGenerator)->setOrigin({ 150, 75, 0 });
 	sys->registerGenerator(wGenerator);
 
 	trailGenerator = new TrailGenerator(this, "Rastro");
@@ -564,7 +564,7 @@ void Scene2::init()
 	pGen = new ProjectileGenerator(this);
 
 	// -- Force generators
-	torbellino = new WhirlGenerator({ 150,80,0 }, 60, this, { 0, -100, 100 }, 0.03);
+	torbellino = new WhirlGenerator({ 150,100,0 }, 60, this, { 0, -10, 10 }, 0.05);
 	sys->registerForceGenerator(torbellino);
 	sysRb->registerForceGenerator(torbellino);
 
